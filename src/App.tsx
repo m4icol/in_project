8e336c9cbbc0e9ed3d25b2c41d1e73b7b0631068
Maickol <m4icol.r12@gmail.com>
2024-10-19 13:11:00 -0500

    in_project

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="bg-blue-500 text-white p-4 rounded">
+      <h1 className="text-2xl font-bold">Hola, Tailwind en React!</h1>
+    </div>
+  );
+}
+
+export default App;
